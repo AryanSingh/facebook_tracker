f7104cf61946f32ff99a216d7df86de10b9557ae
AryanSingh <aryan@iitgn.ac.in>
2015-10-26 15:27:33 +0530

    setting up devise

--- app/controllers/pull_requests_controller.rb
+++ app/controllers/pull_requests_controller.rb
@@ -1,17 +1,17 @@
 class PullRequestsController < ApplicationController
   def new
-      @pull_request = PullRequest.new
-    end
+    @pull_request = PullRequest.new
+  end
 
-    def create
-      @pull_request = PullRequest.new(pull_request_params)
-      @pull_request.save!
-      redirect_to pull_requests_path
-    end
+  def create
+    @pull_request = PullRequest.new(pull_request_params)
+    @pull_request.save!
+    redirect_to pull_requests_path
+  end
 
-    def index
-      @pull_requests = PullRequest.all
-    end
+  def index
+    @pull_requests = PullRequest.all
+  end
 
 private
   def pull_request_params
